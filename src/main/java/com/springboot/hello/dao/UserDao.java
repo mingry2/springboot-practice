5a2b997aae59f550eafd5b4c76f1a41241fe3beb
minkyoung kim <minry20415@naver.com>
2022-10-28 13:57:23 +0900

    [add] User, UserDao 추가

---
+++ src/main/java/com/springboot/hello/dao/UserDao.java
@@ -0,0 +1,56 @@
+package com.springboot.hello.dao;
+
+import com.springboot.hello.domain.dao.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+
+@Component
+public class UserDao {
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"),
+                    rs.getString("name"),
+                    rs.getString("password"));
+            return user;
+        }
+    };
+
+    public void add(User user) throws SQLException {
+        jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES(?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public void deleteAll() {
+        jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() throws SQLException {
+        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+
+    public User findById(String id) {
+        String sql = "SELECT id, name, password FROM users WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<User> getAll() {
+        String sql = "SELECT id, name, password FROM users order by id";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+}
